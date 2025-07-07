5b3a54427d2acdf14bb72e415a795a2f82dfcfaf
zlima410 <endercraft68@gmail.com>
2025-07-06 19:07:34 -0500

    task: Create method for getting length of streak for a habit

--- Models/Habit.cs
+++ Models/Habit.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace StatTrak.Models;
 
 public class Habit
@@ -9,4 +11,26 @@ public class Habit
     public string Color { get; set; } = "4CAF50";
     public DateTime CreatedDate { get; set; } = DateTime.Now;
     public List<HabitEntry> Entries { get; set; } = new List<HabitEntry>();
+
+    public int GetStreakCount()
+    {
+        var sortedEntries = Entries.OrderByDescending(e => e.Date).ToList();
+        int streak = 0;
+        var currentDate = DateTime.Today;
+
+        foreach (var entry in sortedEntries)
+        {
+            if (entry.Date.Date == currentDate.Date && entry.IsCompleted)
+            {
+                streak++;
+                currentDate = currentDate.AddDays(-1);
+            }
+            else if (entry.Date.Date < currentDate.Date)
+            {
+                break;
+            }
+        }
+
+        return streak;
+    }
 }
